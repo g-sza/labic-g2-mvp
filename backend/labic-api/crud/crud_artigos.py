f76f00a4a4194e1183a8c367ae4c9f63abae975e
Igo <igofer1435@gmail.com>
2026-06-15 01:36:21 -0300

    fix: resolve erro na query onde ficava models.artigos e models.projetos nao localizava no banco e agora esta models.ProjetoModel e models.ArtigoModel

--- backend/labic-api/crud/crud_artigos.py
+++ backend/labic-api/crud/crud_artigos.py
@@ -2,26 +2,26 @@ from sqlalchemy.orm import Session
 import models, schemas
 
 def get_artigo(db: Session, artigo_id: int):
-    return db.query(models.artigos).filter(models.artigos.id_artigo == artigo_id).first()
+    return db.query(models.ArtigoModel).filter(models.ArtigoModel.id_artigo == artigo_id).first()
 
 def get_artigos(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.artigos).offset(skip).limit(limit).all()
+    return db.query(models.ArtigoModel).offset(skip).limit(limit).all()
 
 def create_artigo(db: Session, artigo: schemas.ArtigoCreate):
-    db_artigo = models.artigos(**artigo.model_dump())
+    db_artigo = models.ArtigoModel(**artigo.model_dump())
     db.add(db_artigo)
     db.commit()
     db.refresh(db_artigo)
     return db_artigo
 
-def update_artigo(db: Session, db_artigo: models.artigos, artigo_atualizado: schemas.ArtigoCreate):
+def update_artigo(db: Session, db_artigo: models.ArtigoModel, artigo_atualizado: schemas.ArtigoCreate):
     for key, value in artigo_atualizado.model_dump().items():
         setattr(db_artigo, key, value)
     db.commit()
     db.refresh(db_artigo)
     return db_artigo
 
-def delete_artigo(db: Session, db_artigo: models.artigos):
+def delete_artigo(db: Session, db_artigo: models.ArtigoModel):
     db.delete(db_artigo)
     db.commit()
     return True
