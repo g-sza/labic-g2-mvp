a468e59eeda9c29d7f5f15ad758ec613f42727b8
miqueas-andrade <miqueas.andrade.dev@outlook.com>
2026-06-22 12:04:25 -0300

    configura rotas do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,64 @@
-import { useState } from 'react'
+// Componente principal com rotas
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
+
+// Páginas públicas
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import LinhasPesquisa from './pages/LinhasPesquisa'
 import Contato from './pages/Contato'
+import Login from './pages/Login'
+
+// Páginas privadas (precisa estar logado)
 import Dashboard from './pages/Dashboard'
-import Formulario from './pages/Formulario'
+import NovoPesquisador from './pages/NovoPesquisador'
+import NovoProjeto from './pages/NovoProjeto'
+import NovoArtigo from './pages/NovoArtigo'
 
 function App() {
-  // página sendo exibida
-  const [pagina, setPagina] = useState('home')
-
-  // exibindo página certa
-  function renderizarPagina() {
-    if (pagina === 'home') return <Home setPagina={setPagina} />
-    if (pagina === 'sobre') return <Sobre />
-    if (pagina === 'linhas') return <LinhasPesquisa />
-    if (pagina === 'contato') return <Contato />
-    if (pagina === 'dashboard') return <Dashboard />
-    if (pagina === 'formulario') return <Formulario />
-    return <Home setPagina={setPagina} />
-  }
-
   return (
-    <>
-      <Navbar pagina={pagina} setPagina={setPagina} />
+    <BrowserRouter>
+      <Navbar />
       <main className="container">
-        {renderizarPagina()}
+        <Routes>
+          {/* Rotas públicas */}
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/linhas-pesquisa" element={<LinhasPesquisa />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/login" element={<Login />} />
+          
+          {/* Rotas privadas */}
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
+          
+          <Route path="/novo-pesquisador" element={
+            <PrivateRoute>
+              <NovoPesquisador />
+            </PrivateRoute>
+          } />
+          
+          <Route path="/novo-projeto" element={
+            <PrivateRoute>
+              <NovoProjeto />
+            </PrivateRoute>
+          } />
+          
+          <Route path="/novo-artigo" element={
+            <PrivateRoute>
+              <NovoArtigo />
+            </PrivateRoute>
+          } />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
