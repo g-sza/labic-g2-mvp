4e91e5253d3abf50c638b864080de9c5452eb3b6
g-sza <souzasantosguilherme84@gmail.com>
2026-07-20 13:25:54 -0300

    feat: verificação de admin em rotas de projetos

--- backend/labic-api/routers/projetos_router.py
+++ backend/labic-api/routers/projetos_router.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from crud import crud_projetos
 from schemas.projetos_schema import ProjetoCreate, ProjetoUpdate, ProjetoResponse
+from core.security import verificar_permissao_admin
 
 router = APIRouter(prefix="/projetos", tags=["Projetos"])
 
@@ -18,18 +19,18 @@ def buscar_projeto(id: int, db: Session = Depends(get_db)):
     return db_projeto
 
 @router.post("/", response_model=ProjetoResponse, status_code=status.HTTP_201_CREATED)
-def criar_projeto(projeto: ProjetoCreate, db: Session = Depends(get_db)):
+def criar_projeto(projeto: ProjetoCreate, db: Session = Depends(get_db), admin=Depends(verificar_permissao_admin)):
     return crud_projetos.create_projeto(db=db, projeto=projeto)
 
 @router.put("/{id}", response_model=ProjetoResponse)
-def atualizar_projeto(id: int, projeto_atualizado: ProjetoUpdate, db: Session = Depends(get_db)):
+def atualizar_projeto(id: int, projeto_atualizado: ProjetoUpdate, db: Session = Depends(get_db), admin=Depends(verificar_permissao_admin)):
     db_projeto = crud_projetos.get_projeto(db=db, projeto_id=id)
     if db_projeto is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Projeto não encontrado")
     return crud_projetos.update_projeto(db=db, db_projeto=db_projeto, projeto_atualizado=projeto_atualizado)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def deletar_projeto(id: int, db: Session = Depends(get_db)):
+def deletar_projeto(id: int, db: Session = Depends(get_db), admin=Depends(verificar_permissao_admin)):
     db_projeto = crud_projetos.get_projeto(db=db, projeto_id=id)
     if db_projeto is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Projeto não encontrado")
@@ -41,7 +42,8 @@ def associar_pesquisador_ao_projeto(
     id_projeto: int, 
     id_pesquisador: int, 
     papel: str = "Participante", 
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    admin=Depends(verificar_permissao_admin)
 ):
     crud_projetos.adicionar_pesquisador_projeto(
         db=db, 
@@ -55,7 +57,8 @@ def associar_pesquisador_ao_projeto(
 def desassociar_pesquisador_do_projeto(
     id_projeto: int, 
     id_pesquisador: int, 
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    admin=Depends(verificar_permissao_admin)
 ):
     sucesso = crud_projetos.remover_pesquisador_projeto(
         db=db, 
