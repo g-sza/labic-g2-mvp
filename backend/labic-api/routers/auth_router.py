5506af2126e7738aaf58eb1f84ee87098c5064ef
g-sza <souzasantosguilherme84@gmail.com>
2026-07-20 12:04:43 -0300

    feat: utiliza segurança implementada na autenticação

--- backend/labic-api/routers/auth_router.py
+++ backend/labic-api/routers/auth_router.py
@@ -1,11 +1,39 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+# imports internos
+from database import get_db
+from models.pesquisadores import PesquisadorModel
+from core.security import verificar_senha, criar_token_acesso
 
 router = APIRouter(prefix="/auth", tags=["Autenticação"])
 
 @router.post("/login")
-def login(dados: dict):
-    return {"mensagem": "Rota de login - será implementada na fase 2"}
+def login(dados_login: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    # busca o usuário no banco de dados usando o email
+    usuario = db.query(PesquisadorModel).filter(PesquisadorModel.email == dados_login.username).first()
+
+    if not usuario or not verificar_senha(dados_login.password, usuario.senha_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="E-mail ou senha incorretos",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # prepara dados do token
+    dados_token = {
+        "sub": str(usuario.id_pesquisador)
+    }
+
+    # gera token
+    token = criar_token_acesso(dados_usuario=dados_token)
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
 
 @router.post("/logout")
 def logout():
-    return {"mensagem": "Rota de logout - será implementada na fase 2"}
+    return {"mensagem": "Logout realizado."}
