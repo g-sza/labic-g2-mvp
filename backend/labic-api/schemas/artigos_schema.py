07304a862946c0dc604d15685b885d35e2eb882b
g-sza <souzasantosguilherme84@gmail.com>
2026-07-20 13:34:24 -0300

    feat: verificação condicional para criação de artigo

--- backend/labic-api/schemas/artigos_schema.py
+++ backend/labic-api/schemas/artigos_schema.py
@@ -1,10 +1,9 @@
 from datetime import date
 from typing import Optional
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, model_validator
 from models.artigos import StatusArtigo
 
-
-#Classe base que será usado em outras classes
+# Classe base que será usado em outras classes
 class ArtigoBase(BaseModel):
     titulo: str
     resumo: Optional[str] = None
@@ -14,13 +13,43 @@ class ArtigoBase(BaseModel):
     data_publicacao: Optional[date] = None
     status: StatusArtigo = StatusArtigo.ANDAMENTO
 
+    # Validação condicional baseada no status
+    @model_validator(mode='after')
+    def validar_campos_por_status(self):
+        
+        # Regra para artigos fora do Rascunho
+        if self.status != StatusArtigo.RASCUNHO:
+            erros_base = []
+            if not self.resumo:
+                erros_base.append("resumo")
+            if not self.metodologia:
+                erros_base.append("metodologia")
+            if not self.revisao_bibliografica:
+                erros_base.append("revisão bibliográfica")
+                
+            if erros_base:
+                campos = ", ".join(erros_base)
+                raise ValueError(f"Os campos {campos} são obrigatórios para artigos em andamento ou publicados.")
 
-#Schema de Criação
+        # Regra exclusiva para artigos Publicados
+        if self.status == StatusArtigo.PUBLICADO:
+            erros_publicacao = []
+            if not self.data_publicacao:
+                erros_publicacao.append("data de publicação")
+            if not self.arquivos_url:
+                erros_publicacao.append("URL do arquivo")
+                
+            if erros_publicacao:
+                campos = " e ".join(erros_publicacao)
+                raise ValueError(f"Os campos {campos} são obrigatórios quando o artigo está com status 'Publicado'.")
+                
+        return self
+
+# Schema de Criação
 class ArtigoCreate(ArtigoBase):
     pass
 
-
-#Schema de Atualização
+# Schema de Atualização
 class ArtigoUpdate(BaseModel):
     titulo: Optional[str] = None
     resumo: Optional[str] = None
@@ -37,12 +66,9 @@ class PesquisadorArtigoResponse(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-#Schema de Resposta
+# Schema de Resposta
 class ArtigoResponse(ArtigoBase):
     id_artigo: int
     pesquisadores_associacao: list[PesquisadorArtigoResponse] = []
 
     model_config = ConfigDict(from_attributes=True)
-
-
-
